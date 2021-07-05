67dcc145a646d7034805449b89097efaa0512436
Tyler Holmes <tyler@holmesengineering.com>
2021-07-05 15:11:39 -0500

    switch to RTIC, start from atsamd blinky example

--- pensel/memory.x
+++ pensel/memory.x
@@ -1,8 +1,7 @@
 MEMORY
 {
-  /* Leave 16k for the default bootloader on the Feather M4 */
-  FLASH (rx) : ORIGIN = 0x00000000 + 16K, LENGTH = 512K - 16K
-  RAM (xrw)  : ORIGIN = 0x20000000, LENGTH = 192K
+  /* Leave 8k for the default bootloader on the Feather M0 */
+  FLASH (rx) : ORIGIN = 0x00000000 + 8K, LENGTH = 256K - 8K
+  RAM (xrw)  : ORIGIN = 0x20000000, LENGTH = 32K
 }
 _stack_start = ORIGIN(RAM) + LENGTH(RAM);
-
--- pensel/src/main.rs
+++ pensel/src/main.rs
@@ -2,16 +2,62 @@
 #![no_main]
 
 use panic_halt as _;
-// use panic_itm as _; // logs messages over ITM; requires ITM support
+use rtic;
 
-use cortex_m::asm;
-use cortex_m_rt::entry;
+extern crate feather_m0 as bsp;
 
-#[entry]
-fn main() -> ! {
-    asm::nop(); // To not have main optimize to abort in release mode, remove when you add code
+#[rtic::app(device = bsp::pac, peripherals = true, dispatchers = [EVSYS])]
+mod app {
 
-    loop {
-        // your code goes here
+    use bsp::hal;
+    use hal::clock::{ClockGenId, ClockSource, GenericClockController};
+    use hal::gpio::v2 as gpio;
+    use hal::pac::Peripherals;
+    use hal::prelude::*;
+    use hal::rtc::{Count32Mode, Rtc};
+    use rtic_monotonic::Extensions;
+
+    #[resources]
+    struct Resources {
+        red_led: gpio::Pin<gpio::PA17, gpio::Output<gpio::PushPull>>,
+    }
+
+    #[monotonic(binds = RTC, default = true)]
+    type RtcMonotonic = Rtc<Count32Mode>;
+
+    #[init]
+    fn init(cx: init::Context) -> (init::LateResources, init::Monotonics) {
+        // setup some basic peripherals...
+        let mut peripherals: Peripherals = cx.device;
+        let pins = bsp::Pins::new(peripherals.PORT);
+        let mut core: rtic::export::Peripherals = cx.core;
+        let mut clocks = GenericClockController::with_external_32kosc(
+            peripherals.GCLK,
+            &mut peripherals.PM,
+            &mut peripherals.SYSCTRL,
+            &mut peripherals.NVMCTRL,
+        );
+        // We can use the RTC in standby for maximum power savings
+        core.SCB.set_sleepdeep();
+
+        // ... configure RTC
+        let rtc_clock_src = clocks
+            .configure_gclk_divider_and_source(ClockGenId::GCLK2, 1, ClockSource::XOSC32K, false)
+            .unwrap();
+        clocks.configure_standby(ClockGenId::GCLK2, true);
+        let rtc_clock = clocks.rtc(&rtc_clock_src).unwrap();
+        let rtc = Rtc::count32_mode(peripherals.RTC, rtc_clock.freq(), &mut peripherals.PM);
+
+        // ... Red LED to blink
+        let red_led: bsp::RedLed = pins.d13.into();
+        blink::spawn().unwrap();
+
+        (init::LateResources { red_led }, init::Monotonics(rtc))
+    }
+
+    #[task(resources = [red_led])]
+    fn blink(mut _cx: blink::Context) {
+        _cx.resources.red_led.lock(|led| led.toggle().unwrap());
+        blink::spawn_after(1_u32.seconds()).ok();
     }
 }
