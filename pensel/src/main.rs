98f64b050db06658a1e9f4837984f94fbbf81a94
Tyler Holmes <tyler@holmesengineering.com>
2021-08-22 16:05:59 -0700

    add some basic BNO055 calibration & the ability to pause execution until serial console is open (currently bypassed)

--- pensel/src/main.rs
+++ pensel/src/main.rs
@@ -11,6 +11,32 @@ use hal::{clock::GenericClockController, delay::Delay, prelude::*};
 use pac::{interrupt, CorePeripherals, Peripherals};
 
 static mut USB_SERIAL: Option<UsbSerial> = None;
+static SHOULD_START: core::sync::atomic::AtomicBool = core::sync::atomic::AtomicBool::new(true);
+
+const BNO055_CALIBRATION: bno055::BNO055Calibration = bno055::BNO055Calibration {
+    acc_offset_x_lsb: 2,
+    acc_offset_x_msb: 0,
+    acc_offset_y_lsb: 252,
+    acc_offset_y_msb: 255,
+    acc_offset_z_lsb: 231,
+    acc_offset_z_msb: 255,
+    mag_offset_x_lsb: 215,
+    mag_offset_x_msb: 254,
+    mag_offset_y_lsb: 174,
+    mag_offset_y_msb: 1,
+    mag_offset_z_lsb: 228,
+    mag_offset_z_msb: 1,
+    gyr_offset_x_lsb: 1,
+    gyr_offset_x_msb: 0,
+    gyr_offset_y_lsb: 0,
+    gyr_offset_y_msb: 0,
+    gyr_offset_z_lsb: 0,
+    gyr_offset_z_msb: 0,
+    acc_radius_lsb: 232,
+    acc_radius_msb: 3,
+    mag_radius_lsb: 241,
+    mag_radius_msb: 2,
+};
 
 #[entry]
 fn main() -> ! {
@@ -50,12 +76,19 @@ fn main() -> ! {
         NVIC::unmask(interrupt::USB);
     }
 
+    while SHOULD_START.load(core::sync::atomic::Ordering::Acquire) == false {
+        cortex_m::asm::wfi();
+    }
+
     // initialize the IMU
     let mut imu = bno055::Bno055::new(i2c).with_alternative_address();
     if let Err(err) = imu.init(&mut delay) {
         handle_bno_err(&err, &mut delay);
     }
     imu.set_mode(bno055::BNO055OperationMode::NDOF, &mut delay)
+        .unwrap();
+
+    imu.set_calibration_profile(BNO055_CALIBRATION, &mut delay)
         .unwrap();
 
     // perpetually read out angle data
@@ -108,6 +141,9 @@ fn USB() {
         if let Some(serial) = USB_SERIAL.as_mut() {
             let bytes_read = serial.poll(&mut buf);
             serial.write(&buf[0..bytes_read]);
+            if bytes_read != 0 {
+                SHOULD_START.store(true, core::sync::atomic::Ordering::Release);
+            }
         }
     }
 }
